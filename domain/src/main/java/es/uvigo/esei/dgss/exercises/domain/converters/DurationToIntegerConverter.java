92102b59685f29c98b779685a9015134ad6d1f41
Alejandro González García <27727839+aggarcia3@users.noreply.github.com>
2021-09-25 01:29:42 +0200

    DurationToIntegerConverter: dumb down to use long instead of BigInteger
    The old Hibernate MySQL 5 dialect used by the application does not
    properly support converters for basic fields that are of type
    BigInteger, even though that type is recognized by the JPA
    specification.
    
    Therefore, dumb down the converter to use a long, instead of a integer
    with arbitrary precision. This comes at the cost that durations longer
    than 2^63 - 1 nanoseconds (292.47 years) can no longer be represented.
    This should be an acceptable compromise in practice, given the
    inconvenience of storing raw blobs more directly.

--- domain/src/main/java/es/uvigo/esei/dgss/exercises/domain/converters/DurationToIntegerConverter.java
+++ domain/src/main/java/es/uvigo/esei/dgss/exercises/domain/converters/DurationToIntegerConverter.java
@@ -1,27 +1,19 @@
 package es.uvigo.esei.dgss.exercises.domain.converters;
 
-import java.math.BigInteger;
 import java.time.Duration;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 @Converter(autoApply = true)
-public class DurationToIntegerConverter implements AttributeConverter<Duration, BigInteger> {
-	private static final BigInteger NS_IN_SECOND = BigInteger.valueOf(1000000000);
-
+public class DurationToIntegerConverter implements AttributeConverter<Duration, Long> {
 	@Override
-	public BigInteger convertToDatabaseColumn(Duration attribute) {
-		return BigInteger.valueOf(attribute.getSeconds()).multiply(NS_IN_SECOND)
-				.add(BigInteger.valueOf(attribute.getNano()));
+	public Long convertToDatabaseColumn(Duration attribute) {
+		return attribute.toNanos();
 	}
 
 	@Override
-	public Duration convertToEntityAttribute(BigInteger dbData) {
-		BigInteger[] durationFields = dbData.divideAndRemainder(NS_IN_SECOND);
-		long seconds = durationFields[0].longValueExact();
-		long nanoseconds_in_second = durationFields[1].longValueExact();
-
-		return Duration.ofSeconds(seconds).plusNanos(nanoseconds_in_second);
+	public Duration convertToEntityAttribute(Long dbData) {
+		return Duration.ofNanos(dbData);
 	}
 }
