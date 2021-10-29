cab1439920044f869a2edf2a500fba718d6e2c6c
Alejandro González García <27727839+aggarcia3@users.noreply.github.com>
2021-10-29 14:24:15 +0200

    UserProfileController: replace confusing annotations with @Transactional

--- web/src/main/java/es/uvigo/esei/dgss/exercises/jsf/controllers/UserProfileController.java
+++ web/src/main/java/es/uvigo/esei/dgss/exercises/jsf/controllers/UserProfileController.java
@@ -3,10 +3,10 @@ package es.uvigo.esei.dgss.exercises.jsf.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
 import es.uvigo.esei.dgss.exercises.domain.Post;
 import es.uvigo.esei.dgss.exercises.domain.User;
@@ -14,15 +14,6 @@ import es.uvigo.esei.dgss.exercises.jsf.dto.PostDTO;
 import es.uvigo.esei.dgss.exercises.service.UserEJB;
 
 @Named("userProfileController")
-@Stateful
-// I think this scope limits the lifetime of the object, even though it is stateful
-// and mixing and matching annotations from different specifications is ugly.
-// I need it to be a EJB because a transaction is needed to convert database entities
-// to DTOs (more precisely in this case, view models). The service layer could do
-// this conversion, but I don't think it's nice from a design perspective to couple
-// the service layer with whatever auxiliary objects we use to show entities, so we
-// do that here even though it doesn't seem like the specifications cover this
-// usage of these annotations clearly
 @RequestScoped
 public class UserProfileController {
 	@Inject
@@ -36,6 +27,7 @@ public class UserProfileController {
 		return login;
 	}
 
+	@Transactional
 	public void setLogin(String login) {
 		this.login = login;
 		this.user = users.get(login);
